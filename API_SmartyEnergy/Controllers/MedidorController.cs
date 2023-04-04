118b42685bd66514db1d3d8618c6648b728301fb
Igor12x <igorbtenorioidi@gmail.com>
2023-04-04 18:53:52 -0300

    Adicionar arquivos de projeto.

---
+++ API_SmartyEnergy/Controllers/MedidorController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using API_SmartyEnergy.Models;
+using Newtonsoft.Json.Linq;
+namespace API_SmartyEnergy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedidorController : ControllerBase
+    {
+        // GET: api/<ArduinoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ArduinoController>/5
+        [HttpGet("{id}")]
+        public IActionResult BuscarConsumo(int id)
+        {
+            /*transformando um array em um Json no formarto string, pois é possível converter para um JArray
+            um objeto do tipo Json*/
+            string json = JsonConvert.SerializeObject(Medidor.buscarConsumo(id)); //provisóriamente o id que pretendemos receber é o código, mas deve mudar para chava estrangeira resdiência
+            return Ok(json); // codigo 200 sucesso
+        }
+
+        // POST api/<ArduinoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ArduinoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ArduinoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API_SmartyEnergy/Models/Medidor.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace API_SmartyEnergy.Models
+{
+    public class Medidor
+    {
+        //string de conexão
+        static MySqlConnection conexao = new MySqlConnection("server=localhost ;database=db_smart_energy ;user id=root; password=9133");
+
+        //precisamos incluir o atribudo da chave estrangeira residência
+        public int codigo;
+        public double consumo;
+        public string registro_dia; //espera receber apenas a data
+        public string registro_horario; //aqui apenas o horário
+
+        public Medidor(int codigo, double consumo, string registro_dia, string registro_horario)
+        {
+            this.codigo = codigo;
+            this.consumo = consumo;
+            this.registro_dia = registro_dia;
+            this.registro_horario = registro_horario;
+        }   
+
+        internal static object buscarConsumo(int id)
+        {
+            try
+            {
+                conexao.Open();
+                MySqlCommand qry = new MySqlCommand(
+                    "SELECT * FROM MEDIDOR WHERE codigo = @cod", conexao);
+                qry.Parameters.AddWithValue("@cod", id);
+
+                List<Medidor> lista = new List<Medidor>();
+
+                MySqlDataReader leitor = qry.ExecuteReader();
+
+                if (leitor.Read())
+                {
+                    /*criei variavéis apenas para organizar melhor, é possível colocar o leitor 
+                     * dentro dos parâmetros do objeto Arduino*/
+
+                    int codigo = int.Parse(leitor["codigo"].ToString());
+                    double consumo = double.Parse(leitor["consumo"].ToString());
+                    string registro_dia = (leitor["registro_dia"].ToString());
+                    string registro_horario = (leitor["registro_horario"].ToString());
+
+                    Medidor a = new Medidor(codigo, consumo, registro_dia, registro_horario);
+
+                    lista.Add(a);
+                } // fim do if
+
+                conexao.Close();
+
+                return lista;
+
+            }
+            catch (Exception e)
+            {
+
+                /*precisamos alterar o método para retornar um object,
+                 *pois retornando uma array de Arduino não é possível devolver uma mensagem de erro para ser identificado*/
+                return e.Message;
+            }
+        }
+    }
+}
