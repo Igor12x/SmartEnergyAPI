8ab0014d7e8292d80b09c4c6e2d1ee764dca17df
Joseph Santos Pereira <100167354+JosephSantos742@users.noreply.github.com>
2023-04-10 22:12:26 -0300

    [feat] FaturaController

---
+++ API_SmartyEnergy/Controllers/FaturaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartAPI.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FaturaController : ControllerBase {
+
+        [HttpGet("UltimaFatura/{id}")]
+        public IActionResult buscarValorUltimaFatura(int id) {
+            string json = JsonConvert.SerializeObject(Fatura.buscarValorUltimaFatura(id));
+            return Ok(json);
+        }
+
+    }
+}
