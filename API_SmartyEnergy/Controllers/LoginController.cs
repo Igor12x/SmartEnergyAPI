772816b52b2d72239438d15217a0e6abaefb220d
Joseph Santos Pereira <100167354+JosephSantos742@users.noreply.github.com>
2023-04-25 19:24:31 -0300

    [feat] Controller de Login

---
+++ API_SmartyEnergy/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartEnergyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartEnergyAPI.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase {
+        [HttpPost]
+        public IActionResult Login([FromBody] Login login) {
+            Cliente cliente = (Cliente)login.validarLogin(login);
+            string json = JsonConvert.SerializeObject(cliente); 
+            return Ok(json); 
+        }
+
+    }
+}
