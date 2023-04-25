4b4a92b341afde47f315b2168e75ad4ed5c44dec
Joseph Santos Pereira <100167354+JosephSantos742@users.noreply.github.com>
2023-04-25 19:29:41 -0300

    [feat] Controller para realizar cadastro

---
+++ API_SmartyEnergy/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using API_SmartEnergy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_SmartEnergy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase {
+        [HttpPost]
+        public void Post([FromBody] Cliente cliente) { 
+            Cadastro.Cadastrar(cliente);
+    }
+    }
+}
