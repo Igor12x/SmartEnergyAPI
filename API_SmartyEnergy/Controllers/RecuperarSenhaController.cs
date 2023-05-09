1ecf96258996e9e52954640a45a48a737dec761c
Joseph Santos Pereira <100167354+JosephSantos742@users.noreply.github.com>
2023-05-09 16:51:12 -0300

    [feat] Controller para recuperar senha

---
+++ API_SmartyEnergy/Controllers/RecuperarSenhaController.cs
@@ -0,0 +1,18 @@
+﻿using API_SmartyEnergy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_SmartyEnergy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecuperarSenhaController : ControllerBase
+    {
+
+        [HttpGet("CodigoVerificacao/{email}")]
+        public IActionResult EnviarCodigoVerificacao(string email)
+        {
+            string codigoVericacao = RecuperarSenha.EnviarCodigoVerificacao(email);
+            return Ok(codigoVericacao);
+        }
+    }
+}
