a5660c82fb97d1bf007615c6ed12aefbb3978b24
JosephSantos742 <josephsantos742@gmail.com>
2023-06-01 19:29:57 -0300

    [fix] Refatoração Model e Controller de fatura

--- API_SmartyEnergy/Controllers/FaturaController.cs
+++ API_SmartyEnergy/Controllers/FaturaController.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using SmartAPI.Models;
 
 namespace SmartAPI.Controllers {
-
     [ApiController]
     [Route("api/[controller]")]
     public class FaturaController : ControllerBase {
-
         [HttpGet("UltimaFatura/{id}")]
-        public IActionResult buscarValorUltimaFatura(int id)
-        {
-            string json = JsonConvert.SerializeObject(Fatura.buscarValorUltimaFatura(id));
-            return Ok(json);
-
+        public IActionResult BuscarValorUltimaFatura(int id) {
+            try {
+                Fatura fatura = Fatura.BuscarValorUltimaFatura(id);
+                return Ok(fatura);
+            } catch (Exception ex) {
+                return StatusCode(500, $"Erro ao buscar a última fatura: {ex.Message}");
+            }
         }
     }
 }
--- API_SmartyEnergy/Models/Fatura.cs
+++ API_SmartyEnergy/Models/Fatura.cs
@@ -1,51 +1,47 @@
 ﻿using MySql.Data.MySqlClient;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartAPI.Models {
     public class Fatura {
-        private string valorUltimaFatura;
-        private string consumoUltimaFatura;
-        public string ValorUltimaFatura{ get => valorUltimaFatura; set => valorUltimaFatura = value; }
-        public string ConsumoUltimaFatura { get => consumoUltimaFatura; set => consumoUltimaFatura = value; }
+        [Required(ErrorMessage = "O campo Valor da Última Fatura é obrigatório.")]
+        public string ValorUltimaFatura { get; set; }
 
-        static MySqlConnection conexao = new MySqlConnection("server=esn509vmysql ;database=smartenergy;user id=aluno; password=Senai1234");
+        [Required(ErrorMessage = "O campo Consumo da Última Fatura é obrigatório.")]
+        public string ConsumoUltimaFatura { get; set; }
 
         public Fatura(string valorUltimaFatura, string consumoUltimaFatura) {
             ValorUltimaFatura = valorUltimaFatura;
             ConsumoUltimaFatura = consumoUltimaFatura;
         }
 
+        internal static Fatura BuscarValorUltimaFatura(int id) {
+            using (MySqlConnection conexao = new MySqlConnection("server=esn509vmysql ;database=smartenergy;user id=aluno; password=Senai1234")) {
+                try {
+                    conexao.Open();
 
-        internal static Fatura buscarValorUltimaFatura(int id) {
-            try {
+                    MySqlCommand qry = new MySqlCommand(
+                        $@"SELECT valor, (SELECT MAX(medicao_mes_atual) FROM fatura)-(SELECT MAX(medicao_mes_anterior) FROM fatura) AS consumoFaturaAltural
+                   FROM fatura
+                   WHERE CODIGO = (SELECT MAX(codigo) FROM fatura) and FK_RESIDENCIA_codigo = @cod;", conexao);
 
-                conexao.Open();
-             
-                MySqlCommand qry = new MySqlCommand(
-                    $@"SELECT valor, (SELECT MAX(medicao_mes_atual) FROM fatura)-(SELECT MAX(medicao_mes_anterior) FROM fatura) AS consumoFaturaAltural
-                    FROM fatura
-                    WHERE CODIGO = (SELECT MAX(codigo) FROM fatura) and FK_RESIDENCIA_codigo = @cod;", conexao);
+                    qry.Parameters.AddWithValue("@cod", id);
 
-                qry.Parameters.AddWithValue("@cod", id);
+                    using (MySqlDataReader leitor = qry.ExecuteReader()) {
+                        if (leitor.Read()) {
+                            string valorUltimaFatura = leitor["valor"].ToString();
+                            string consumoUltimaFatura = leitor["consumoFaturaAltural"].ToString();
 
-                MySqlDataReader leitor = qry.ExecuteReader();
-
-                if (leitor.Read()) {
-                    string valorUltimaFatura = leitor["valor"].ToString();
-                    string consumoUltimaFatura = leitor["consumoFaturaAltural"].ToString();
-
-                    return new Fatura(valorUltimaFatura, consumoUltimaFatura);
-                } else {
-                    throw new Exception("Não foi encontrada nenhuma fatura para o código especificado.");
+                            return new Fatura(valorUltimaFatura, consumoUltimaFatura);
+                        } else {
+                            throw new Exception("Não foi encontrada nenhuma fatura para o código de residência especificado.");
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new Exception("Erro ao buscar a última fatura: " + e.Message);
                 }
-            } catch (Exception e) {
-
-                throw new Exception("Erro ao buscar a última fatura.", e);
-
-            } finally {
-                if(conexao != null) {
-                    conexao.Close();
-                }                
             }
         }
+
     }
 }
