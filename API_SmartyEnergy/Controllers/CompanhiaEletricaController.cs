ab5c7ed763bc73e11fa5419f93b78435d2dc9222
JosephSantos742 <josephsantos742@gmail.com>
2023-06-01 19:20:52 -0300

    [fix] Refatoração Model e Controller da Companhia Elétrica

--- API_SmartyEnergy/Controllers/CompanhiaEletricaController.cs
+++ API_SmartyEnergy/Controllers/CompanhiaEletricaController.cs
@@ -1,21 +1,20 @@
 ﻿using API_SmartyEnergy.Models;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace API_SmartyEnergy.Controllers
-{
+namespace API_SmartyEnergy.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompanhiaEletricaController : ControllerBase
-    {
-        // GET: api/<ValuesController>
-        [HttpGet ("buscarCompanhia/{id}")]
-        public IActionResult buscarCompanhia(int id)
-        {
-            string json = JsonConvert.SerializeObject(CompanhiaEletrica.buscar(id));
-            return Ok(json);
+    public class CompanhiaEletricaController : ControllerBase {
+        [HttpGet("buscarCompanhia/{id}")]
+        public IActionResult BuscarCompanhia(int id) {
+            try {
+                CompanhiaEletrica companhiaEletrica = CompanhiaEletrica.Buscar(id);
+                return Ok(companhiaEletrica);
+            } catch (Exception ex) {
+                return StatusCode(500, $"Erro ao buscar a companhia elétrica: {ex.Message}");
+            }
         }
     }
 }
--- API_SmartyEnergy/Models/CompanhiaEletrica.cs
+++ API_SmartyEnergy/Models/CompanhiaEletrica.cs
@@ -1,67 +1,59 @@
 ﻿using MySql.Data.MySqlClient;
+using System;
+using System.ComponentModel.DataAnnotations;
 
+namespace API_SmartyEnergy.Models {
+    public class CompanhiaEletrica {
+        [Required(ErrorMessage = "O campo TarifaTUSD é obrigatório.")]
+        public double TarifaTUSD { get; set; }
 
-namespace API_SmartyEnergy.Models
-{
-    public class CompanhiaEletrica
-    {
-        private double tarifaTUSD;
-        private double tarifaTE;
-        private double pis;
-        private double cofins;
-        private double icms;
+        [Required(ErrorMessage = "O campo TarifaTE é obrigatório.")]
+        public double TarifaTE { get; set; }
 
-        public double TarifaTUSD { get => tarifaTUSD; set => tarifaTUSD = value; }
-        public double TarifaTE { get => tarifaTE; set => tarifaTE = value; }
-        public double Pis { get => pis; set => pis = value; }
-        public double Cofins { get => cofins; set => cofins = value; }
-        public double Icms { get => icms; set => icms = value; }
+        [Required(ErrorMessage = "O campo Pis é obrigatório.")]
+        public double Pis { get; set; }
 
-        public CompanhiaEletrica(double tarifaTUSD, double tarifaTE, double pis, double cofins, double icms)
-        {
+        [Required(ErrorMessage = "O campo Cofins é obrigatório.")]
+        public double Cofins { get; set; }
+
+        [Required(ErrorMessage = "O campo ICMS é obrigatório.")]
+        public double Icms { get; set; }
+
+        public CompanhiaEletrica(double tarifaTUSD, double tarifaTE, double pis, double cofins, double icms) {
             TarifaTUSD = tarifaTUSD;
             TarifaTE = tarifaTE;
             Pis = pis;
             Cofins = cofins;
             Icms = icms;
         }
-        internal static CompanhiaEletrica buscar(int id)
-        {
-            MySqlConnection conexao = new MySqlConnection("server=esn509vmysql; database=smartenergy; user id=aluno; password=Senai1234");
-            try
-            {
-                conexao.Open();
 
-                MySqlCommand qry = new MySqlCommand(
-                "SELECT * FROM companhia c INNER JOIN residencia r ON c.codigo = r.FK_COMPANHIA_codigo WHERE r.codigo = @cod", conexao);
+        internal static CompanhiaEletrica Buscar(int id) {
+            string connectionString = "server=esn509vmysql; database=smartenergy; user id=aluno; password=Senai1234";
 
-                qry.Parameters.AddWithValue("@cod", id);
+            using (MySqlConnection conexao = new MySqlConnection(connectionString)) {
+                try {
+                    conexao.Open();
 
-                MySqlDataReader leitor = qry.ExecuteReader();
+                    MySqlCommand qry = new MySqlCommand(
+                        "SELECT * FROM companhia c INNER JOIN residencia r ON c.codigo = r.FK_COMPANHIA_codigo WHERE r.codigo = @cod", conexao);
 
-                if (leitor.Read())
-                {
-                    return new CompanhiaEletrica(double.Parse(leitor["tarifa_TUSD"].ToString()),
-                                                double.Parse(leitor["tarifa_TE"].ToString()),
-                                                double.Parse(leitor["pis"].ToString()),
-                                                double.Parse(leitor["cofins"].ToString()),
-                                                double.Parse(leitor["icms"].ToString())
-                                                );
-                }
-                else
-                {
-                    throw new Exception("Não foi encontrada nenhuma companhia para o código especificado.");
-                }
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Erro ao buscar companhia.", e);
-            }
-            finally
-            {
-                if (conexao != null)
-                {
-                    conexao.Close();
+                    qry.Parameters.AddWithValue("@cod", id);
+
+                    using (MySqlDataReader leitor = qry.ExecuteReader()) {
+                        if (leitor.Read()) {
+                            return new CompanhiaEletrica(
+                                double.Parse(leitor["tarifa_TUSD"].ToString()),
+                                double.Parse(leitor["tarifa_TE"].ToString()),
+                                double.Parse(leitor["pis"].ToString()),
+                                double.Parse(leitor["cofins"].ToString()),
+                                double.Parse(leitor["icms"].ToString())
+                            );
+                        } else {
+                            throw new Exception("Não foi encontrada nenhuma companhia para o código especificado.");
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new Exception("Erro ao buscar companhia.", e);
                 }
             }
         }
